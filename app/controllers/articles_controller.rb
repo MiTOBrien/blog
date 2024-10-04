51d9d0cc3d93c3729bf4b078e0915c4f0f33cd7c
Michael OBrien <obrien.michael.t@gmail.com>
2024-10-04 15:52:48 -0400

    Adds basic authentication for editing/deleting posta and comments

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "root", password: "The2ndAmendment!", except: [:index, :show]
+  
   def index
     @articles = Article.all
   end
